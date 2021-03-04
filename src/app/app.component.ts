3ee9d60bd483e3648f81f9d76860ecce58a34ae1
imneverdied <imneverdied@gmail.com>
2021-03-04 09:30:04 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,10 @@ export class AppComponent {
   name = "Angular";
   BuyFee = 0.001425;
   Discount = 0.28;
+  tax = 0.003;
   Stack = 1;
   BPrice = 0;
+  SPrice = 0;
   Amount = 0;
   BuyFeeCount = 0;
 }
